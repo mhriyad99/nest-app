5187d295b8d78b6e969eaa3f4a4d088b2b5597ae
mhriyad99 <mhriyad98@gmail.com>
2026-09-16 17:04:47 +0600

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service.js';
 import { ProductController } from './product/product.controller.js';
 import { ProductService } from './product/product.service.js';
 import { EmployeeModule } from './employee/employee.module.js';
+import { CategoryModule } from './category/category.module.js';
 
 @Module({
-  imports: [EmployeeModule],
+  imports: [EmployeeModule, CategoryModule],
   controllers: [AppController, ProductController],
   providers: [AppService, ProductService],
 })
